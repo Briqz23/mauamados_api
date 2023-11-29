d6a5c411c3057ceb923c1c73119014ca2fb6f406
Briqz23 <dbriquez.devmaua@gmail.com>
2023-11-29 16:38:51 -0300

    rota criar 1 usuário

--- routes/user_route.py
+++ routes/user_route.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, APIRouter, Response, Query, Path, FastAPI
 import json
 from config.database import collection_name_user
 from models.user_model import User, UpdateUser
-from schemas.user_schema import users_serializer
+from schemas.user_schema import user_serializer, users_serializer
 from bson import ObjectId
 from passlib.context import CryptContext
 from services.services import is_user_over_eighteen, validar_login, validate_password
@@ -54,12 +54,30 @@ async def create_users(users: list[User]):
 
     return users_serializer(created_users)
 
+@user_api_router.post("/user")
+async def create_user(user: User):
+    if not is_user_over_eighteen(user.age):
+        raise HTTPException(status_code=400, detail="Apenas usuários maiores de 18 podem criar a conta")
 
+    if not validate_password(user.senha):
+        raise HTTPException(status_code=400, detail="Senha deve ter ao menos 8 caracteres")
+    
+    if not validar_login(user.login):
+        raise HTTPException(status_code=400, detail="login deve ter @maua.br")
 
-@user_api_router.post("/users")
-async def create_users(users: list[User]):
-    _ids = collection_name_user.insert_many([dict(user) for user in users])
-    return users_serializer(collection_name_user.find({"_id": {"$in": _ids.inserted_ids}}))
+    # Hash da senha usando bcrypt
+    hashed_password = password_context.hash(user.senha)
+    
+    # Substituir a senha do usuário pelo hash gerado
+    user.senha = hashed_password
+
+    _id = collection_name_user.insert_one(dict(user))
+    created_user = collection_name_user.find_one({"_id": _id.inserted_id})
+
+    def send_mail():
+        print("Um email de segurança foi enviado para o @maua.br do usuário")
+
+    return user_serializer(created_user)  # replace with your actual serializer function
 
 @user_api_router.delete("/delete_all_users")
 async def delete_all_users():
