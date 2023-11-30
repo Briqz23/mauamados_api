ff22a10ee4f0fdf60c7ffe2b2f3de3b6acd84545
Briqz23 <dbriquez.devmaua@gmail.com>
2023-11-30 10:33:18 -0300

    fix pytest do kusko

--- tests/test_user_route.py
+++ tests/test_user_route.py
@@ -1,10 +1,10 @@
 import pytest
 from fastapi import HTTPException
-from routes.user_route import create_users
+from routes.user_route import create_user, create_users
 
 class TestCreateUsers:
-    @pytest.mark.asyncio
-    async def test_create_users_valid_data(self):
+    
+    def test_create_users_valid_data(self):
         valid_user_data = [
             {
                 "ma_id": 1,
@@ -19,27 +19,25 @@ class TestCreateUsers:
             }
         ]
 
-        result = await create_users(valid_user_data)
+        result = create_users(valid_user_data)
         assert result
 
-    @pytest.mark.asyncio
-    async def test_create_users_underage(self):
-        underage_user_data = [
-            {
-                "ma_id": 2,
-                "name": "Jane Doe",
-                "age": 16,
-                "course": "Engineering",
-                "genero": "Feminino",
-                "sexual_orientation": "Heterossexual",
-                "tags_preferences": ["tag1", "tag2"],
-                "login": "jane.doe@maua.br",
-                "senha": "securepassword",
-            }
-        ]
+
+    def test_create_underage_user():
+        underage_user = {
+            "ma_id": 2,
+            "name": "Jane Doe",
+            "age": 16,
+            "course": "Engineering",
+            "genero": "Feminino",
+            "sexual_orientation": "Heterossexual",
+            "tags_preferences": ["tag1", "tag2"],
+            "login": "jane.doe@maua.br",
+            "senha": "securepassword",
+        }
 
         with pytest.raises(HTTPException) as exc_info:
-            await create_users(underage_user_data)
+            create_user(underage_user) 
         assert exc_info.value.status_code == 400
-
-
+            
+        
